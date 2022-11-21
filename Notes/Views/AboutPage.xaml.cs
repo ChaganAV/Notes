7de6f5b1d7d52b5b0f2f7ffda61870cffb5295a7
Alexandr <jwchaganav@gmail.com>
2022-11-21 14:35:41 +0400

    создал тестовый проект

---
+++ Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
+        }
+    }
+}
---
+++ Notes/Views/NotesPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        public NotesPage()
+        {
+            InitializeComponent();
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName)) File.Delete(_fileName);
+            editor.Text = string.Empty;
+        }
+    }
+}
